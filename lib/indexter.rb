50a76d48ad80fac9276e84ba2b3b186825b8feae
Chris Cummer <chris@unbounce.com>
2017-02-23 11:35:38 -0800

    Rake task now accepts formatter as a param.
    Examples:
    
    $ bundle exec rake 'indexter:validate'        => default formatter (PassFail)
    $ bundle exec rake 'indexter:validate[json]'  => JSON formatter
    $ bundle exec rake 'indexter:validate[table]' => ASCII table formatter

--- lib/indexter.rb
+++ lib/indexter.rb
@@ -3,6 +3,7 @@ require 'indexter/validator'
 require 'indexter/railtie' if defined?(Rails)
 
 require 'indexter/formatters/json'
+require 'indexter/formatters/pass_fail'
 require 'indexter/formatters/table'
 
 module Indexter
--- lib/indexter/railtie.rb
+++ lib/indexter/railtie.rb
@@ -8,13 +8,18 @@ module Indexter
     rake_tasks do
       namespace :indexter do
         desc "Checks the database for missing foreign key indexes"
-        task validate: :environment do
-          formatter = Indexter::Formatters::Table
-          puts Indexter::Validator.new(formatter: formatter).validate
+        task :validate, [:formatter] => :environment do |t, args| 
+          puts Indexter::Validator.new(formatter: formatter(formatter: args[:formatter])).validate
         end
       end
 
       task indexter: ['indexter:validate']
+    end
+
+    def formatter(formatter: nil)
+      formatter  = 'pass_fail' unless formatter
+      klass_name = "Indexter::Formatters::#{formatter.to_s.camelize}"
+      klass      = klass_name.constantize
     end
   end
 end
